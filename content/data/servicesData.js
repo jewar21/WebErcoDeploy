c7d48c16c6250836e53bc073a808eb53b95c43cc
JGuerrero-at-014991514483 <jguerrero@ercoenergia.com.co>
2022-02-28 11:49:33 -0500

    Data services, missing iconos

--- content/data/servicesData.js
+++ content/data/servicesData.js
@@ -1,60 +1,188 @@
+import {
+  RiComputerLine,
+  RiLeafLine,
+  RiCustomerService2Line,
+  RiShieldStarLine,
+  RiGroupLine,
+  RiTrophyLine
+} from "react-icons/ri";
+
+import imghome1 from "../../assets/imagenes/Hogar/HogarInterconectado.png";
+import imghome2 from "../../assets/imagenes/Hogar/HogarHibrido.png";
+import imghome3 from "../../assets/imagenes/Hogar/HogarAisldo.png";
+import imgCompany1 from "../../assets/imagenes/Empresas/EmpresaInterconectado.png";
+import imgCompany2 from "../../assets/imagenes/Empresas/EmpresaHibrido.png";
+import imgCompany3 from "../../assets/imagenes/Empresas/EmpresaAislado.png";
+
+const iconComputer = <RiComputerLine />;
+const iconLeaf = <RiLeafLine />;
+const iconCustomer = <RiCustomerService2Line />;
+const iconShield = <RiShieldStarLine />;
+const iconGroup = <RiGroupLine />;
+
 // cover info
 
 export const converinfo = {
-    title: "Hogar",
-    subtitle: "Energía solar",
-    content: "Elige una de nuestras soluciones de energía solar y comienza a construir tu ecosistema de energía. Así podrás tener ahorros adicionales y el control de toda tu energía. "
-}
+  title: "Hogar",
+  subtitle: "Energía solar",
+  content:
+    "Elige una de nuestras soluciones de energía solar y comienza a construir tu ecosistema de energía. Así podrás tener ahorros adicionales y el control de toda tu energía. "
+};
 
+// instalación
 
-import {
-    RiComputerLine,
-    RiLeafLine,
-    RiCustomerService2Line,
-    RiShieldStarLine,
-    RiGroupLine,
-    RiTrophyLine,
-  } from "react-icons/ri";
-  
-  
-  const iconComputer = <RiComputerLine />;
-  const iconLeaf = <RiLeafLine />;
-  const iconCustomer = <RiCustomerService2Line />;
-  const iconShield = <RiShieldStarLine />;
-  const iconGroup = <RiGroupLine />;
-  const iconTrophy = <RiTrophyLine />;
-  
-  // Home Experience
-  
-  export const experienceContent = [
+export const homeInstallation = {
+  title: "Instalación",
+  content:
+    "Instala un sistema de energía solar fotovoltaico para tu hogar y genera hasta el 100% de tus consumos energéticos.",
+  cards: [
+    {
+      img: imghome1,
+      title: "Sistema interconectado",
+      content:
+        "Estos sistemas funcionan en conjunto con la red eléctrica convencional, te permiten un flujo constante de energía y conectar cualquier equipo de tu hogar sin importar su potencia o consumo. Los sistemas interconectados no generan un respaldo de energía."
+    },
+    {
+      img: imghome2,
+      title: "Sistema Híbrido",
+      content:
+        "Está compuesto por un sistema de energía solar y un banco de baterías que te permiten almacenar la energía producida por los paneles solares y utilizarla en cualquier momento del día, durante la noche o en caso de cortes de la energía de la red convencional."
+    },
+    {
+      img: imghome3,
+      title: "Sistema Aislado",
+      content:
+        "Utiliza un banco de baterías para almacenar la energía eléctrica producida por los paneles solares, este sistema no requiere de la red eléctrica convencional para funcionar. Esta energía la puedes utilizar en cualquier momento del día o durante la noche."
+    }
+  ]
+};
+
+export const companyInstallation = {
+  title: "Instalación",
+  content:
+    "Instala un sistema de energía solar fotovoltaico para tu empresa y mejora su productividad y eficiencia energética.",
+  cards: [
+    {
+      img: imgCompany1,
+      title: "Sistema interconectado",
+      content:
+        "Son sistemas ideales para que tu empresa pueda generar ahorros hasta del 100% de la factura de energía, recuperar la inversión entre 4 a 6 años, contribuir a un futuro sostenible y acceder a beneficios tributarios. Este sistema no genera un respaldo de energía."
+    },
+    {
+      img: imgCompany2,
+      title: "Sistema Híbrido",
+      content:
+        "Si tienes un servicio de energía inestable y deseas tener un respaldo de energía y al mismo tiempo generar ahorros en la factura de energía, el sistema híbrido es el ideal, ya que combina el sistema interconectado con el aislado, permitiendo utilizar la energía producida en cualquier momento del día o durante la noche."
+    },
+    {
+      img: imgCompany3,
+      title: "Sistema Aislado",
+      content:
+        "Si tu empresa se encuentra en una zona rural, está apartada de la red eléctrica, tiene energía inestable y necesita de un respaldo de energía ¡el sistema aislado es el ideal para ti! Este sistema almacena la energía producida en baterías que puedes utilizar en cualquier momento del día o durante la noche."
+    }
+  ]
+};
+
+// NEU+
+
+export const neuPlus = {
+  title: "NEU+",
+  description:
+    "Visualiza tus consumos de energía en tiempo real, recibe notificaciones a través de inteligencia artificial.",
+  cards: [
     {
       icon: iconCustomer,
-      title: "Acompañamiento permanente",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      title: "Visualización",
+      content: "De datos de consumo y generación en tiempo real."
     },
     {
       icon: iconShield,
-      title: "Garantía",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      title: "Notificaciones",
+      content: "Recibe alertas por consumos atípicos."
     },
     {
       icon: iconGroup,
-      title: "Equipos de calidad",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      title: "Alta tecnología",
+      content: "Para la gestión inteligente de tus proyectos."
     },
     {
       icon: iconComputer,
-      title: "Tecnología",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      title: "Evita multas",
+      content: "Por consumo excesivo de energía reactiva."
     },
     {
       icon: iconLeaf,
-      title: "Medio ambiente",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      title: "Facturación",
+      content: "Paga tu factura desde la App o automatiza tus pagos."
+    }
+  ]
+};
+
+// Operación y mantenimiento
+
+export const operationAndMaintenance = {
+  title: "Operación & Mantenimiento",
+  description:
+    "Garantizamos que tu sistema solar trabaje al punto máximo de operación.",
+  cards: [
+    {
+      icon: iconCustomer,
+      title: "Calidad",
+      content:
+        "Garantizamos que tu sistema solar trabaje al punto máximo de operación."
     },
     {
-      icon: iconTrophy,
-      title: "Trayectoria",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      icon: iconShield,
+      title: "Análisis",
+      content: "Del comportamiento y eficiencia de tu proyecto solar."
+    },
+    {
+      icon: iconGroup,
+      title: "Mantenimiento",
+      content: "Realizamos mantenimiento predictivo, preventivo y correctivo."
+    },
+    {
+      icon: iconComputer,
+      title: "Personal",
+      content: "Contamos con un equipo técnico propio y calificado."
     }
-  ];
+  ]
+};
+
+// Banner 5 steps
+
+export const stepsSolarEnergy = [
+  {
+    number: "01",
+    description: "Firma de contrato",
+    info: "Relacionamiento inicial, introducción y entrega de documentos."
+  },
+  {
+    number: "02",
+    description: "Diseño e ingeniería",
+    info: "Visita de ingeniería, ingeniería de detalle y compra de equipos."
+  },
+  {
+    number: "03",
+    description: "instalación",
+    info: "De estructuras, paneles, inversor e instalación de acometidas DC, AC."
+  },
+  {
+    number: "04",
+    description: "RETIE",
+    info: "Envío de documentación, visita de certificación y certificación."
+  },
+  {
+    number: "05",
+    description: "Tu primer kilovatio",
+    info: "Entrega de tu sistema solar y capacitación de la App de monitoreo."
+  }
+];
+
+// Payment Methods
+
+export const paymentMethods = {
+  title: "Formas de pago",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit:",
+  methods: "Forma de pago 1, forma de pago 2, forma de pago 3."
+};
