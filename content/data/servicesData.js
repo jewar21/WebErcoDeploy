16d1e5fc238fb36304ffc0183246bb2fe592c0aa
JGuerrero-at-014991514483 <jguerrero@ercoenergia.com.co>
2022-03-08 19:44:02 -0500

    ServicesData update

--- content/data/servicesData.js
+++ content/data/servicesData.js
@@ -22,9 +22,18 @@ const iconGroup = <RiGroupLine />;
 
 // cover info
 
-export const converinfo = {
-  title: "Hogar",
-  subtitle: "Energía solar",
+export const homeConverInfo = {
+  img: "",
+  section: "Hogar",
+  title: "Energía solar",
+  content:
+    "Elige una de nuestras soluciones de energía solar y comienza a construir tu ecosistema de energía. Así podrás tener ahorros adicionales y el control de toda tu energía. "
+};
+
+export const companyConverInfo = {
+  img: "",
+  section: "Empresa",
+  title: "Energía solar",
   content:
     "Elige una de nuestras soluciones de energía solar y comienza a construir tu ecosistema de energía. Así podrás tener ahorros adicionales y el control de toda tu energía. "
 };
@@ -32,7 +41,10 @@ export const converinfo = {
 // instalación
 
 export const homeInstallation = {
-  title: "Instalación",
+  title: "Instalación energía solar",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel lectus tortor. Fusce ut dolor dolor. Morbi viverra bibendum risus a imperdiet.",
+  subtitle: "Instalación",
   content:
     "Instala un sistema de energía solar fotovoltaico para tu hogar y genera hasta el 100% de tus consumos energéticos.",
   cards: [
@@ -40,25 +52,94 @@ export const homeInstallation = {
       img: imghome1,
       title: "Sistema interconectado",
       content:
-        "Estos sistemas funcionan en conjunto con la red eléctrica convencional, te permiten un flujo constante de energía y conectar cualquier equipo de tu hogar sin importar su potencia o consumo. Los sistemas interconectados no generan un respaldo de energía."
+        "Estos sistemas funcionan en conjunto con la red eléctrica convencional, te permiten un flujo constante de energía y conectar cualquier equipo de tu hogar sin importar su potencia o consumo. Los sistemas interconectados no generan un respaldo de energía.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     },
     {
       img: imghome2,
       title: "Sistema Híbrido",
       content:
-        "Está compuesto por un sistema de energía solar y un banco de baterías que te permiten almacenar la energía producida por los paneles solares y utilizarla en cualquier momento del día, durante la noche o en caso de cortes de la energía de la red convencional."
+        "Está compuesto por un sistema de energía solar y un banco de baterías que te permiten almacenar la energía producida por los paneles solares y utilizarla en cualquier momento del día, durante la noche o en caso de cortes de la energía de la red convencional.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     },
     {
       img: imghome3,
       title: "Sistema Aislado",
       content:
-        "Utiliza un banco de baterías para almacenar la energía eléctrica producida por los paneles solares, este sistema no requiere de la red eléctrica convencional para funcionar. Esta energía la puedes utilizar en cualquier momento del día o durante la noche."
+        "Utiliza un banco de baterías para almacenar la energía eléctrica producida por los paneles solares, este sistema no requiere de la red eléctrica convencional para funcionar. Esta energía la puedes utilizar en cualquier momento del día o durante la noche.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     }
   ]
 };
 
 export const companyInstallation = {
-  title: "Instalación",
+  title: "Instalación energía solar",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel lectus tortor. Fusce ut dolor dolor. Morbi viverra bibendum risus a imperdiet.",
+  subtitle: "Instalación",
   content:
     "Instala un sistema de energía solar fotovoltaico para tu empresa y mejora su productividad y eficiencia energética.",
   cards: [
@@ -66,19 +147,85 @@ export const companyInstallation = {
       img: imgCompany1,
       title: "Sistema interconectado",
       content:
-        "Son sistemas ideales para que tu empresa pueda generar ahorros hasta del 100% de la factura de energía, recuperar la inversión entre 4 a 6 años, contribuir a un futuro sostenible y acceder a beneficios tributarios. Este sistema no genera un respaldo de energía."
+        "Son sistemas ideales para que tu empresa pueda generar ahorros hasta del 100% de la factura de energía, recuperar la inversión entre 4 a 6 años, contribuir a un futuro sostenible y acceder a beneficios tributarios. Este sistema no genera un respaldo de energía.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     },
     {
       img: imgCompany2,
       title: "Sistema Híbrido",
       content:
-        "Si tienes un servicio de energía inestable y deseas tener un respaldo de energía y al mismo tiempo generar ahorros en la factura de energía, el sistema híbrido es el ideal, ya que combina el sistema interconectado con el aislado, permitiendo utilizar la energía producida en cualquier momento del día o durante la noche."
+        "Si tienes un servicio de energía inestable y deseas tener un respaldo de energía y al mismo tiempo generar ahorros en la factura de energía, el sistema híbrido es el ideal, ya que combina el sistema interconectado con el aislado, permitiendo utilizar la energía producida en cualquier momento del día o durante la noche.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     },
     {
       img: imgCompany3,
       title: "Sistema Aislado",
       content:
-        "Si tu empresa se encuentra en una zona rural, está apartada de la red eléctrica, tiene energía inestable y necesita de un respaldo de energía ¡el sistema aislado es el ideal para ti! Este sistema almacena la energía producida en baterías que puedes utilizar en cualquier momento del día o durante la noche."
+        "Si tu empresa se encuentra en una zona rural, está apartada de la red eléctrica, tiene energía inestable y necesita de un respaldo de energía ¡el sistema aislado es el ideal para ti! Este sistema almacena la energía producida en baterías que puedes utilizar en cualquier momento del día o durante la noche.",
+      items: [
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        },
+        {
+          icon: iconComputer,
+          title: "Lorem ipsum",
+          text: "Activa alarmas de consumo para enterarte en tiempo real del consumo alcanzado."
+        }
+      ]
     }
   ]
 };
