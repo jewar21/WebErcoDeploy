a2a7db77d309212a5a88d6aa4275501a569f9cdf
jewar21 <jesseard94@gmail.com>
2022-05-03 06:41:26 -0500

    ContactUs update full views

--- components/Contact/ContactComponent.js
+++ components/Contact/ContactComponent.js
@@ -1,15 +1,15 @@
-import React from "react";
+// components
 import QuoteButton from "../PartialComponents/QuoteButton";
 
 const ContactComponent = ({ contactData }) => {
   const cards = contactData.cards;
   return (
-    <section className="relative w-screen bg-lightGrey-300">
+    <section className="contactContent">
       <div className="contactContainer">
         <h3>{contactData.header}</h3>
         <h2>{contactData.title}</h2>
         <p>{contactData.content}</p>
-        <div className="lg:flex w-full lg:justify-between">
+        <div className="contentCardContainer">
           {cards.map((card, i) => {
             const icon = card.icon;
             const title = card.title;
@@ -22,9 +22,7 @@ const ContactComponent = ({ contactData }) => {
                 <p>{info}</p>
                 <QuoteButton
                   buttonParameters={
-                    i === 0
-                      ? "w-12.5rem h-12 px-2 font-semibold bg-primary-500 text-darkGrey-900"
-                      : "h-12 w-12.5rem lg:h-12 border-primary-500 border-solid border-2 rounded-full text-darkGrey-900 font-semibold"
+                    i === 0 ? "contactButton" : "contactButtonT"
                   }
                   buttonText={buttonText}
                   isIcon={false}
--- components/Contact/Offices.js
+++ components/Contact/Offices.js
@@ -1,36 +1,51 @@
 import Image from "next/image";
 
 const Offices = ({ contactData }) => {
+  console.log("esteeee", contactData);
   const title = contactData.title;
   const contacts = contactData.contacts;
   const img1 = contactData.img1.src;
   const img2 = contactData.img2.src;
+
+  const iconMail = contactData.iconMail;
+  const iconPhone = contactData.iconPhone;
+
   return (
-    <section className="w-screen">
-      <div className="px-6 lg:px-5.25rem lg:py-32">
-        <h4 className="text-H4 font-bold text-darkGrey-900 py-12 lg:py-0">{title}</h4>
-        {contacts.map((contact, i) => {
-          const country = contact.country;
-          const email = contact.email;
-          const phone = contact.phone;
-          const iconMail = contact.iconMail;
-          const iconPhone = contact.iconPhone;
-          return (
-            <div key={i} className="">
-              <h5 className="text-Subtitle font-Regular text-darkGrey-900">
-                {country}
-              </h5>
-              <div className="flex items-center">
-                <div>{iconMail}</div>
-                <span>{email}</span>
-              </div>
-              <div className="flex items-center">
-                <div className="">{iconPhone}</div>
-                <span>{phone}</span>
-              </div>
-            </div>
-          );
-        })}
+    <section className="officesContent">
+      <div className="officesCoverImage">
+        <Image
+          src={img2}
+          alt="image"
+          layout="fill"
+          objectFit="cover"
+          priority="true"
+        />
+      </div>
+      <div className="lg:w-full lg:flex lg:flex-col lg:items-end">
+        <div className="officesContainer">
+          <h4>{title}</h4>
+          <div className="officesInfo">
+            {contacts.map((contact, i) => {
+              console.log(contact);
+              const country = contact.country;
+              const email = contact.email;
+              const phone = contact.phone;
+              return (
+                <div key={i}>
+                  <h5 className="officesInfoTitle">{country}</h5>
+                  <div className="officesCenter">
+                    <div className="officesIcon">{iconMail}</div>
+                    <span>{email}</span>
+                  </div>
+                  <div className="officesCenter">
+                    <div className="officesIcon">{iconPhone}</div>
+                    <span>{phone}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <div className="relative w-screen h-[19.5rem] lg:hidden">
         <div className="absolute w-full h-full top-0 left-0">
