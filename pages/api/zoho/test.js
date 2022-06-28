bbb76e76239a3abb28a7cd454fc32ed712b5d7d3
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2022-06-28 16:51:06 -0500

    Updated test

--- pages/api/zoho/test.js
+++ pages/api/zoho/test.js
@@ -1,24 +1,26 @@
+import nextConnect from 'next-connect';
+
 import { ZohoAPI } from '../../../utils/zoho/config'
 
 const zoho = new ZohoAPI();
 
+const test = nextConnect({
+    // Handle any other HTTP method
+    onNoMatch(req, res) {
+        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+    },
+})
 
-const handler = async (req, res) => {
+// Process a POST request
+test.get( async (req, res) => {
+    const data = await zoho.getModuleRecord()
 
-    if(req.method === 'GET') {
-        const data = await zoho.getModuleRecord()
-    
-        const response = {
-            message: 'Hola Mundo',
-            data
-        }
-    
-        res.status(200).json(response)
-    } else {
-        res.status(500).json({
-            message: `Method ${req.method} not allowed`
-        })
+    const response = {
+        message: 'Hola Mundo',
+        data
     }
-}
+    
+    res.status(200).json(response);
+});
 
-export default handler;
+export default test;
